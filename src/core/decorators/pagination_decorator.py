e0344c12543ec83e834852915a47f591f8895a0a
hmarcuzzo <henrique.souza.m06@gmail.com>
2022-06-14 14:49:11 +0100

    feat: Search in all columns #13

--- src/core/decorators/pagination_decorator.py
+++ src/core/decorators/pagination_decorator.py
@@ -1,26 +1,25 @@
-from typing import Union
+from typing import TypeVar, Union
 
 from fastapi import Query
-from pydantic.main import ModelMetaclass
-from sqlalchemy import cast, String
-from sqlalchemy.orm import DeclarativeMeta
 
-from src.core.types.exceptions_type import BadRequestException
 from src.core.types.find_many_options_type import FindManyOptions
-from src.core.types.pagination_type import Pagination, PaginationSearch, PaginationSort
 from src.core.utils.pagination_utils import PaginationUtils
+
+E = TypeVar("E")
+T = TypeVar("T")
+F = TypeVar("F")
 
 
 class GetPagination(object):
     def __init__(
         self,
-        entity: DeclarativeMeta,
-        find_all_query: ModelMetaclass = None,
-        group_by_query: ModelMetaclass = None,
+        entity: E,
+        find_all_query: T = None,
+        order_by_query: F = None,
     ):
         self.entity = entity
         self.find_all_query = find_all_query
-        self.order_by_query = group_by_query
+        self.order_by_query = order_by_query
 
     def __call__(
         self,
@@ -32,60 +31,17 @@ class GetPagination(object):
         sort: Union[list[str], None] = Query(
             default=None, regex=".*:(ASC|DESC|\\+|\\-)$", example=["field:by"]
         ),
+        search_all: Union[str, None] = Query(default=None),
     ) -> FindManyOptions:
-        pagination_params: Pagination = Pagination(skip=skip, take=take)
-
-        if sort:
-            pagination_params["sort"] = []
-            for sort_param in sort:
-                sort_param_split = sort_param.split(":")
-                if sort_param_split[1] == "+":
-                    sort_param_split[1] = "ASC"
-                elif sort_param_split[1] == "-":
-                    sort_param_split[1] = "DESC"
-
-                pagination_params["sort"].append(
-                    PaginationSort(field=sort_param_split[0], by=sort_param_split[1])
-                )
-
-            if self.order_by_query and not PaginationUtils.validateSortFilter(
-                pagination_params["sort"], self.order_by_query
-            ):
-                raise BadRequestException("Invalid sort filters")
-
-        if search:
-            pagination_params["search"] = []
-            for search_param in search:
-                search_param_split = search_param.split(":")
-                pagination_params["search"].append(
-                    PaginationSearch(
-                        field=search_param_split[0], value=search_param_split[1]
-                    )
-                )
-
-            if self.find_all_query and not PaginationUtils.validateSearchFilter(
-                pagination_params["search"], self.find_all_query
-            ):
-                raise BadRequestException("Invalid search filters")
-
-        pagination_data = FindManyOptions(
-            skip=(pagination_params["skip"] - 1) * pagination_params["take"],
-            take=pagination_params["take"],
+        paging_params = PaginationUtils.generate_paging_parameters(
+            skip,
+            take,
+            search,
+            sort,
+            self.find_all_query,
+            self.order_by_query,
         )
 
-        if sort:
-            pagination_data["order_by"] = []
-            for sort_param in pagination_params["sort"]:
-                sort_obj = getattr(self.entity, sort_param["field"])
-                sort_func = "asc" if (sort_param["by"] == "ASC") else "desc"
-                pagination_data["order_by"].append(getattr(sort_obj, sort_func)())
-
-        if search:
-            pagination_data["where"] = []
-            for search_param in pagination_params["search"]:
-                search_obj = getattr(self.entity, search_param["field"])
-                pagination_data["where"].append(
-                    cast(search_obj, String).ilike(f'%{search_param["value"]}%')
-                )
-
-        return pagination_data
+        return PaginationUtils.get_paging_data(
+            self.entity, paging_params, search, sort, search_all, self.find_all_query
+        )
