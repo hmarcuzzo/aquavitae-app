5f0906c65c8773ba0a2ce56cdb8fb07bd67ee41a
hmarcuzzo <henrique.souza.m06@gmail.com>
2022-06-15 17:14:45 +0100

    fix: Fix user interface and user service

--- src/modules/infrastructure/user/user_interface.py
+++ src/modules/infrastructure/user/user_interface.py
@@ -15,7 +15,7 @@ class UserInterface:
     async def find_one_user(
         self, db: Session, find_data: FindOneOptions
     ) -> Optional[User]:
-        return await self.user_service.find_one_user(find_data, db)
+        return await self.user_service.get_one_user(find_data, db)
 
     async def update_last_access(self, db: Session, id: str) -> Optional[UpdateResult]:
         return await self.user_service.update_last_access(id, db)
