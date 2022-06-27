dbc0e32fb39996d69875f94ad57f6c15678d7e39
hmarcuzzo <henrique.souza.m06@gmail.com>
2022-06-27 18:12:41 +0100

    refactor: Changing the CORS source location

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from config import APP_ENV, APP_PORT, APP_TZ
+from config import APP_ENV, APP_PORT, APP_TZ, CORS_ORIGINS
 from src.core.handlers.http_exceptions_handler import HttpExceptionsHandler
 from src.core.middlewares.limit_upload_size import LimitUploadSize
 from src.modules.app import app_routers
@@ -13,14 +13,9 @@ from src.modules.app import app_routers
 app = FastAPI(title="Aquavitae App", version="0.0.1")
 
 # CORS
-origins = [
-    "http://localhost",
-    f"http://localhost:{APP_PORT}",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=CORS_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
