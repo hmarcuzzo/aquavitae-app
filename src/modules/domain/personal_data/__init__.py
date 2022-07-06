26017e0595367477b81397185731e5802ae7c84d
hmarcuzzo <henrique.souza.m06@gmail.com>
2022-07-06 09:50:02 +0100

    fix: Activity level entity #29

--- src/modules/domain/personal_data/__init__.py
+++ src/modules/domain/personal_data/__init__.py
@@ -1,5 +1,5 @@
-from .entities.activity_level import ActivityLevel
+from .entities.activity_level_entity import ActivityLevel
 from .controllers.activity_level_controller import activity_level_router
 
-from .entities.personal_data import PersonalData
+from .entities.personal_data_entity import PersonalData
 from .controllers.personal_data_controller import personal_data_router
