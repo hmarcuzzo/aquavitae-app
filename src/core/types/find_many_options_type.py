12ff0239df2d502d880d30e94df03f7c041e8538
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2022-08-02 14:22:20 +0100

    refactor: TypedDict, static methods and style of files

--- src/core/types/find_many_options_type.py
+++ src/core/types/find_many_options_type.py
@@ -1,6 +1,6 @@
 from src.core.types.find_one_options_type import FindOneOptions
 
 
-class FindManyOptions(FindOneOptions):
+class FindManyOptions(FindOneOptions, total=False):
     skip: int
     take: int
--- src/modules/domain/food/services/food_category_service.py
+++ src/modules/domain/food/services/food_category_service.py
@@ -29,13 +29,9 @@ class FoodCategoryService:
     async def create_food_category(
         self, food_category_dto: CreateFoodCategoryDto, db: Session
     ) -> Optional[FoodCategoryDto]:
-        new_food_category = await self.food_category_repository.create(
-            food_category_dto, db
-        )
+        new_food_category = await self.food_category_repository.create(food_category_dto, db)
 
-        new_food_category = await self.food_category_repository.save(
-            new_food_category, db
-        )
+        new_food_category = await self.food_category_repository.save(new_food_category, db)
         return FoodCategoryDto(**new_food_category.__dict__)
 
     async def get_all_food_category_paginated(
@@ -48,10 +44,7 @@ class FoodCategoryService:
         ] = await self.food_category_repository.find_and_count(pagination, db)
 
         return create_pagination_response_dto(
-            [
-                FoodCategoryDto(**food_category.__dict__)
-                for food_category in all_food_categories
-            ],
+            [FoodCategoryDto(**food_category.__dict__) for food_category in all_food_categories],
             total,
             pagination["skip"],
             pagination["take"],
@@ -73,9 +66,7 @@ class FoodCategoryService:
     async def update_food_category(
         self, id: str, update_food_category_dto: UpdateFoodCategoryDto, db: Session
     ) -> Optional[UpdateResult]:
-        return await self.food_category_repository.update(
-            id, update_food_category_dto, db
-        )
+        return await self.food_category_repository.update(id, update_food_category_dto, db)
 
     async def delete_food_category(
         self, food_category_id: str, db: Session
