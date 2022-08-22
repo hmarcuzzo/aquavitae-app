635854665924efe53c04baad758ce4431b19acb4
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2022-08-22 11:38:03 +0100

    feat: Auth tests #32

--- src/modules/infrastructure/auth/test/test_auth_e2e.py
+++ src/modules/infrastructure/auth/test/test_auth_e2e.py
@@ -7,11 +7,11 @@ from starlette.status import HTTP_200_OK, HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FO
 from src.core.constants.enum.user_role import UserRole
 from src.main import app
 from src.modules.infrastructure.auth.dto.login_payload_dto import LoginPayloadDto
-from test.utils.test_base_e2e import TestBaseE2E
+from test.test_base_e2e import TestBaseE2E
 
 
 @pytest.mark.describe(f"GET Route: /login")
-class TestGetAllUsers(TestBaseE2E):
+class TestLogin(TestBaseE2E):
     route = f"/login"
 
     @pytest.mark.asyncio
@@ -64,11 +64,11 @@ class TestGetAllUsers(TestBaseE2E):
 
 
 @pytest.mark.describe(f"GET Route: /me")
-class TestGetUserById(TestBaseE2E):
+class TestGetCurrentUser(TestBaseE2E):
     route = f"/me"
 
     @pytest.mark.asyncio
-    @pytest.mark.it("Success: Get user admin data")
+    @pytest.mark.it("Success: Get current user data")
     async def test_get_current_user(self, user_admin: Optional[LoginPayloadDto]) -> None:
         async with AsyncClient(app=app, base_url=self.base_url) as ac:
             response = await ac.get(
@@ -87,4 +87,4 @@ class TestGetUserById(TestBaseE2E):
     @pytest.mark.asyncio
     @pytest.mark.it("Failure: Get current user without authentication")
     async def test_no_authentication(self) -> None:
-        await self.no_authentication(self.route)
+        await self.get_no_authentication(self.route)
