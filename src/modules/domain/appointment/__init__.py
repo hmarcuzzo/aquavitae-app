9c6b40f104686dc6974f78943308be3219aeedd5
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2022-09-27 16:15:31 +0100

    refactor: Changing the ordering of routes and entities

--- src/modules/domain/appointment/__init__.py
+++ src/modules/domain/appointment/__init__.py
@@ -1,2 +1,10 @@
+from fastapi import APIRouter
+
 from .entities.appointment_goal_entity import AppointmentGoal
 from .controllers.appointment_goal_controller import appointment_goal_router
+
+
+appointment_routers = APIRouter()
+appointment_routers.include_router(appointment_goal_router)
+
+appointment_entities = [AppointmentGoal]
--- src/modules/domain/food/__init__.py
+++ src/modules/domain/food/__init__.py
@@ -1,6 +1,13 @@
+from fastapi import APIRouter
+
 from .entities.food_entity import Food
 from .controllers.food_controller import food_router
 
 from .entities.food_category_entity import FoodCategory
 from .controllers.food_category_controller import food_category_router
 
+food_routers = APIRouter()
+food_routers.include_router(food_router)
+food_routers.include_router(food_category_router)
+
+food_entities = [Food, FoodCategory]
