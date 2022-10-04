852a6cd0d95d0b620940dd6150731d8182c334d2
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2022-10-04 17:50:13 +0100

    refactor: Removing redundant code from pagination utils

--- src/core/utils/pagination_utils.py
+++ src/core/utils/pagination_utils.py
@@ -42,21 +42,18 @@ class PaginationUtils:
             ):
                 raise BadRequestException("Invalid sort filters")
 
+        paging_params["search"] = []
         if search:
-            paging_params["search"] = []
             for search_param in search:
                 search_param_split = search_param.split(":")
                 paging_params["search"].append(
                     PaginationSearch(field=search_param_split[0], value=search_param_split[1])
                 )
 
-            if find_all_query and not PaginationUtils.validateSearchFilter(
-                paging_params["search"], find_all_query
-            ):
-                raise BadRequestException("Invalid search filters")
-        else:
-            if find_all_query and not PaginationUtils.validateSearchFilter([], find_all_query):
-                raise BadRequestException("Invalid search filters")
+        if find_all_query and not PaginationUtils.validateSearchFilter(
+            paging_params["search"], find_all_query
+        ):
+            raise BadRequestException("Invalid search filters")
 
         return paging_params
 
