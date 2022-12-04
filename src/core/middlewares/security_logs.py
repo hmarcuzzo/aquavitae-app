258c39609e2cc68eedee70af737c7ae19716b1fa
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2022-12-04 16:44:23 +0000

    feat: Encrypt data before sending to blockchain

--- src/core/middlewares/security_logs.py
+++ src/core/middlewares/security_logs.py
@@ -1,5 +1,8 @@
+import json
 from datetime import datetime
 
+import cipher
+from cryptography.fernet import Fernet
 from httpx import AsyncClient
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
@@ -16,20 +19,26 @@ class SecurityLogs(BaseHTTPMiddleware):
         self.route = "/api/v1/security-logs"
 
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
-        async with AsyncClient(app=self.app, base_url=self.base_url) as ac:
-            response = await ac.post(
-                self.route,
-                json={
-                    "timestamp": datetime.now().timestamp(),
-                    "method": request.method,
-                    "path": request.url.path,
-                    "user_id": verify_token(
-                        request.headers["Authorization"].split(" ")[1], "user_id"
-                    ),
-                },
-                headers={"Authorization": f"Bearer {'token'}"},
-            )
+        body = {
+            "timestamp": datetime.now().timestamp(),
+            "method": request.method,
+            "path": request.url.path,
+            "user_id": verify_token(request.headers["Authorization"].split(" ")[1], "user_id"),
+        }
 
-        data = response.json()
+        key = Fernet.generate_key()
+        cipher = Fernet(key)
+
+        body_bytes = json.dumps(body).encode("utf-8")
+        body_encrypted = cipher.encrypt(body_bytes)
+
+        # async with AsyncClient(app=self.app, base_url=self.base_url) as ac:
+        #     response = await ac.post(
+        #         self.route,
+        #         json=body_encrypted,
+        #         headers={"Authorization": f"Bearer {'token'}"},
+        #     )
+        #
+        # data = response.json()
 
         return await call_next(request)
