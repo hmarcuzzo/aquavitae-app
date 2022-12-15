65fa0a17421c677170566f41b449b772526c77eb
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2022-12-15 15:04:31 +0000

    feat: saving the log to disk after it is inserted into the blockchain

--- src/modules/blockchain/blockchain_service.py
+++ src/modules/blockchain/blockchain_service.py
@@ -1,8 +1,11 @@
+import json
+import os
 from datetime import datetime
 
 from httpx import AsyncClient
 from starlette.requests import Request
 
+from config import ROOT_DIR
 from src.modules.blockchain.utils import BlockchainUtils
 from src.modules.infrastructure.auth.jwt_service import verify_token
 
@@ -10,6 +13,7 @@ from src.modules.infrastructure.auth.jwt_service import verify_token
 class BlockchainService:
     def __init__(self):
         self.base_url = "http://localhost:5000"
+        self.transactions_log_file = ROOT_DIR + os.path.join("/src/static/doc/system_logs.txt")
 
     # -------------- PUBLIC METHODS --------------
     async def send_new_block(self, request: Request):
@@ -18,15 +22,22 @@ class BlockchainService:
         signed_body = BlockchainUtils.sign(encrypted_body)
 
         try:
+            transaction_body = {
+                "signature": signed_body,
+                "transaction_hash": encrypted_body.decode("utf-8"),
+            }
             async with AsyncClient(base_url=self.base_url) as ac:
                 response = await ac.post(
-                    "/add",
-                    json={
-                        "signature": signed_body,
-                        "transaction_hash": encrypted_body.decode("utf-8"),
-                    },
+                    "/transactions",
+                    json=transaction_body,
                 )
-            # data = response.json()
+
+                if response.status_code == 200:
+                    open(self.transactions_log_file, "w").close() if not os.path.exists(
+                        self.transactions_log_file
+                    ) else None
+                    with open(self.transactions_log_file, "ab") as file_obj:
+                        file_obj.write(json.dumps(transaction_body).encode("utf-8"))
         except Exception:
             pass
 
