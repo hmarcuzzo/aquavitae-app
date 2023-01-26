2937ef6ebae2a613f3019d0a1d0029f9892959fc
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2023-01-26 21:49:44 +0000

    feat: New Item's endpoints

--- src/modules/domain/item/controllers/item_controller.py
+++ src/modules/domain/item/controllers/item_controller.py
@@ -1,13 +1,21 @@
 from typing import Optional
+from uuid import UUID
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from starlette.status import HTTP_201_CREATED
 
+from src.core.common.dto.pagination_response_dto import PaginationResponseDto
 from src.core.constants.enum.user_role import UserRole
 from src.core.decorators.http_decorator import Auth
+from src.core.decorators.pagination_decorator import GetPagination
+from src.core.types.find_many_options_type import FindManyOptions
+from src.core.types.update_result_type import UpdateResult
 from src.modules.domain.item.dto.item.create_item_dto import CreateItemDto
 from src.modules.domain.item.dto.item.item_dto import ItemDto
+from src.modules.domain.item.dto.item.item_query_dto import FindAllItemQueryDto, OrderByItemQueryDto
+from src.modules.domain.item.dto.item.update_item_dto import UpdateItemDto
+from src.modules.domain.item.entities.item_entity import Item
 from src.modules.domain.item.services.item_service import ItemService
 from src.modules.infrastructure.database import get_db
 
@@ -26,3 +34,47 @@ async def create_item(
     request: CreateItemDto, database: Session = Depends(get_db)
 ) -> Optional[ItemDto]:
     return await item_service.create_item(request, database)
+
+
+@item_router.get(
+    "/get",
+    response_model=PaginationResponseDto[ItemDto],
+    response_model_exclude_unset=True,
+    dependencies=[Depends(Auth([UserRole.ADMIN, UserRole.NUTRITIONIST]))],
+)
+async def get_all_item_paginated(
+    pagination: FindManyOptions = Depends(
+        GetPagination(Item, ItemDto, FindAllItemQueryDto, OrderByItemQueryDto)
+    ),
+    database: Session = Depends(get_db),
+) -> Optional[PaginationResponseDto[ItemDto]]:
+    return await item_service.get_all_item_paginated(pagination, database)
+
+
+@item_router.get(
+    "/get/{id}",
+    response_model=ItemDto,
+    dependencies=[Depends(Auth([UserRole.ADMIN, UserRole.NUTRITIONIST]))],
+)
+async def get_item_by_id(id: UUID, database: Session = Depends(get_db)) -> Optional[ItemDto]:
+    return await item_service.find_one_item(str(id), database)
+
+
+@item_router.delete(
+    "/delete/{id}",
+    response_model=UpdateResult,
+    dependencies=[Depends(Auth([UserRole.ADMIN, UserRole.NUTRITIONIST]))],
+)
+async def delete_item(id: UUID, database: Session = Depends(get_db)) -> Optional[UpdateResult]:
+    return await item_service.delete_item(str(id), database)
+
+
+@item_router.patch(
+    "/update/{id}",
+    response_model=UpdateResult,
+    dependencies=[Depends(Auth([UserRole.ADMIN, UserRole.NUTRITIONIST]))],
+)
+async def update_item(
+    request: UpdateItemDto, id: UUID, database: Session = Depends(get_db)
+) -> Optional[UpdateResult]:
+    return await item_service.update_item(str(id), request, database)
--- src/modules/domain/item/services/item_has_food_service.py
+++ src/modules/domain/item/services/item_has_food_service.py
@@ -2,10 +2,14 @@ from typing import List, Optional
 
 from sqlalchemy.orm import Session
 
+from src.core.types.update_result_type import UpdateResult
 from src.modules.domain.item.dto.item_has_food.create_item_has_food_dto import CreateItemHasFoodDto
 from src.modules.domain.item.dto.item_has_food.item_has_food_dto import ItemHasFoodDto
+from src.modules.domain.item.dto.item_has_food.update_item_has_food_dto import UpdateItemHasFoodDto
+from src.modules.domain.item.entities.item_entity import Item
 from src.modules.domain.item.entities.item_has_food_entity import ItemHasFood
 from src.modules.domain.item.repositories.item_has_food_repository import ItemHasFoodRepository
+from src.modules.domain.item.repositories.item_repository import ItemRepository
 
 
 class ItemHasFoodService:
@@ -16,19 +20,63 @@ class ItemHasFoodService:
 
     # ---------------------- INTERFACE METHODS ----------------------
     async def create_item_has_food_interface(
-        self, item_has_foods_dto: List[CreateItemHasFoodDto], db: Session
+        self, item_has_foods: List[CreateItemHasFoodDto], db: Session
     ) -> Optional[List[ItemHasFoodDto]]:
         db.begin_nested()
-        return await self._create_item_has_food(item_has_foods_dto, db)
+        return await self._create_item_has_food(item_has_foods, db)
+
+    async def update_item_has_food_interface(
+        self, update_item_has_foods: List[UpdateItemHasFoodDto], db: Session
+    ) -> Optional[UpdateResult]:
+        return await self._update_item_has_food(update_item_has_foods, db)
 
     # ---------------------- PRIVATE METHODS ----------------------
     async def _create_item_has_food(
-        self, item_has_foods_dto: List[CreateItemHasFoodDto], db: Session
+        self, item_has_foods: List[CreateItemHasFoodDto], db: Session
     ) -> Optional[List[ItemHasFoodDto]]:
         new_item_has_foods: List[ItemHasFood] = []
-        for food in item_has_foods_dto:
+        for food in item_has_foods:
             tmp_item_has_food = await self.item_has_food_repository.create(food, db)
             new_item_has_foods.append(tmp_item_has_food)
 
         new_item_has_foods = await self.item_has_food_repository.save(new_item_has_foods, db)
         return [ItemHasFoodDto(**item_has_food.__dict__) for item_has_food in new_item_has_foods]
+
+    async def _update_item_has_food(
+        self, update_item_has_foods: List[UpdateItemHasFoodDto], db: Session
+    ) -> Optional[UpdateResult]:
+        item = await ItemRepository().find_one_or_fail(
+            {"where": Item.id == update_item_has_foods[0].item_id, "relations": ["foods"]}, db
+        )
+
+        response: UpdateResult = UpdateResult(raw=[], affected=0, generatedMaps=[])
+
+        foods_updated = []
+        for item_food in item.foods:
+            db.begin_nested()
+            updated = False
+            for new_food in update_item_has_foods:
+                # Update foods in the new foods list
+                if item_food.food_id == new_food.food_id:
+                    updated = True
+                    foods_updated.append(new_food)
+                    response["affected"] += (
+                        await self.item_has_food_repository.update(str(item_food.id), new_food, db)
+                    )["affected"]
+
+            # Delete foods not in the new foods list
+            if not updated:
+                response["affected"] += (
+                    await self.item_has_food_repository.soft_delete(str(item_food.id), db)
+                )["affected"]
+
+        # Create new foods in update method
+        db.begin_nested()
+        response["affected"] += len(
+            await self._create_item_has_food(
+                [new_food for new_food in update_item_has_foods if new_food not in foods_updated],
+                db,
+            )
+        )
+
+        return response
