cfa7d7f014c93138f7a6fcacd8b951df4119e405
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2023-02-06 18:56:38 +0000

    refactor: Rearranging code order

--- src/modules/infrastructure/database/base_repository.py
+++ src/modules/infrastructure/database/base_repository.py
@@ -27,144 +27,6 @@ class BaseRepository(Generic[T]):
         self.entity = entity
         self.query_constructor = QueryConstructor(entity)
 
-    # ----------- PRIVATE METHODS -----------
-    def __get_repository_from_foreign_keys(
-        self, entity_data: Union[BaseModel, dict]
-    ) -> List[Tuple["BaseRepository", str, Any]]:
-        foreign_keys = {
-            column.key: column for column in get_columns(self.entity) if column.foreign_keys
-        }
-
-        for key, value in entity_data.items():
-            if key in foreign_keys:
-                referred_table = next(
-                    iter(foreign_keys[key].foreign_keys)
-                ).constraint.referred_table
-
-                referred_repository = BaseRepository(get_class_by_table(Base, referred_table))
-                yield referred_repository, key, value
-
-    async def __is_relations_valid(
-        self, db: Session, partial_entity: Union[BaseModel, dict]
-    ) -> bool:
-        foreign_keys = {
-            column.key: column for column in get_columns(self.entity) if column.foreign_keys
-        }
-
-        for referred_repository, key, value in self.__get_repository_from_foreign_keys(
-            partial_entity
-        ):
-            if not value and foreign_keys[key].nullable:
-                continue
-            await referred_repository.find_one_or_fail(str(value), db)
-
-        return True
-
-    async def __remove_deleted_relations(
-        self, db: Session, result: T, options_dict: FindManyOptions = None
-    ) -> T:
-        result_class = inspect(result).class_
-        result_relationships = inspect(result_class).relationships
-        for key, relation_property in result_relationships.items():
-            if (
-                not options_dict
-                or "relations" not in options_dict
-                or key not in options_dict["relations"]
-            ):
-                for referred_repository, f_key, value in self.__get_repository_from_foreign_keys(
-                    result.__dict__
-                ):
-                    if value:
-                        _entity = await referred_repository.find_one(str(value), db)
-
-                        if _entity is None:
-                            setattr(result, f_key, None)
-
-            else:
-                if getattr(result, key):
-                    column = DatabaseUtils.get_column_represent_deleted(get_columns(result_class))
-                    if not isinstance(getattr(result, key), list):
-                        if getattr(getattr(result, key), column.description):
-                            setattr(result, key, None)
-                        else:
-                            new_result_key = await self.__remove_deleted_relations(
-                                db, getattr(result, key), options_dict
-                            )
-                            setattr(result, key, new_result_key)
-                    else:
-                        for index, element in enumerate(getattr(result, key)):
-                            if getattr(element, column.description):
-                                del getattr(result, key)[index]
-                            else:
-                                new_result_key = await self.__remove_deleted_relations(
-                                    db, element, options_dict
-                                )
-                                getattr(result, key)[index] = new_result_key
-
-        return result
-
-    @staticmethod
-    def __get_cascade_relations(entity: T) -> List[Any]:
-        cascade_relations = []
-
-        for relation in inspect(inspect(entity).class_).relationships:
-            delete_column = DatabaseUtils.get_column_represent_deleted(
-                get_columns(relation.mapper.class_)
-            )
-            if delete_column is None:
-                raise ValueError(f'Relation "{relation.key}" has no "deleted" column')
-
-            cr = getattr(entity, relation.key)
-            if cr:
-                if not isinstance(cr, List):
-                    cr = [cr]
-
-                for cascade_relation in cr:
-                    if relation.cascade.delete_orphan and not getattr(
-                        cascade_relation,
-                        delete_column.name,
-                    ):
-                        cascade_relations.append(cascade_relation)
-
-        return cascade_relations
-
-    async def __soft_delete_cascade_relations(self, entity: T, db: Session) -> int:
-        rowcount = 0
-
-        cascade_entities = self.__get_cascade_relations(entity)
-        for cascade_entity in cascade_entities:
-            result = await BaseRepository(inspect(cascade_entity).class_).__soft_delete_cascade(
-                str(cascade_entity.id), db
-            )
-            rowcount += result["affected"]
-
-        return rowcount
-
-    @staticmethod
-    def __soft_delete_entity(entity: T, db: Session) -> int:
-        entity_class = inspect(entity).class_
-        delete_column = DatabaseUtils.get_column_represent_deleted(get_columns(entity_class))
-        if delete_column is None:
-            raise ValueError(f'Entity "{entity_class.__name__}" has no "deleted" column')
-
-        if not getattr(entity, delete_column.name):
-            setattr(entity, delete_column.name, datetime.now())
-            db.flush()
-
-            return 1
-
-        return 0
-
-    async def __soft_delete_cascade(
-        self, criteria: Union[str, int, FindOneOptions], db: Session
-    ) -> Optional[UpdateResult]:
-        entity = await self.find_one_or_fail(criteria, db)
-
-        rowcount = await self.__soft_delete_cascade_relations(entity, db)
-        rowcount += self.__soft_delete_entity(entity, db)
-
-        return UpdateResult(raw=[], affected=rowcount, generatedMaps=[])
-
     # ----------- PUBLIC METHODS -----------
     async def find(
         self, options_dict: FindManyOptions = None, db: Session = next(get_db())
@@ -306,3 +168,142 @@ class BaseRepository(Generic[T]):
     def refresh_entity(entity: Union[T, List[T]], db: Session) -> Union[T, List[T]]:
         db.refresh(entity) if not isinstance(entity, List) else (db.refresh(_en) for _en in entity)
         return entity
+
+    # ----------- PRIVATE METHODS -----------
+    async def __remove_deleted_relations(
+        self, db: Session, result: T, options_dict: FindManyOptions = None
+    ) -> T:
+        result_class = inspect(result).class_
+        result_relationships = inspect(result_class).relationships
+        for key, relation_property in result_relationships.items():
+            if (
+                not options_dict
+                or "relations" not in options_dict
+                or key not in options_dict["relations"]
+            ):
+                for (
+                    referred_repository,
+                    f_key,
+                    value,
+                ) in self.__get_repository_from_foreign_keys(result.__dict__):
+                    if value:
+                        _entity = await referred_repository.find_one(str(value), db)
+
+                        if _entity is None:
+                            setattr(result, f_key, None)
+
+            else:
+                if getattr(result, key):
+                    column = DatabaseUtils.get_column_represent_deleted(get_columns(result_class))
+                    if not isinstance(getattr(result, key), list):
+                        if getattr(getattr(result, key), column.description):
+                            setattr(result, key, None)
+                        else:
+                            new_result_key = await self.__remove_deleted_relations(
+                                db, getattr(result, key), options_dict
+                            )
+                            setattr(result, key, new_result_key)
+                    else:
+                        for index, element in enumerate(getattr(result, key)):
+                            if getattr(element, column.description):
+                                del getattr(result, key)[index]
+                            else:
+                                new_result_key = await self.__remove_deleted_relations(
+                                    db, element, options_dict
+                                )
+                                getattr(result, key)[index] = new_result_key
+
+        return result
+
+    def __get_repository_from_foreign_keys(
+        self, entity_data: Union[BaseModel, dict]
+    ) -> List[Tuple["BaseRepository", str, Any]]:
+        foreign_keys = {
+            column.key: column for column in get_columns(self.entity) if column.foreign_keys
+        }
+
+        for key, value in entity_data.items():
+            if key in foreign_keys:
+                referred_table = next(
+                    iter(foreign_keys[key].foreign_keys)
+                ).constraint.referred_table
+
+                referred_repository = BaseRepository(get_class_by_table(Base, referred_table))
+                yield referred_repository, key, value
+
+    async def __is_relations_valid(
+        self, db: Session, partial_entity: Union[BaseModel, dict]
+    ) -> bool:
+        foreign_keys = {
+            column.key: column for column in get_columns(self.entity) if column.foreign_keys
+        }
+
+        for referred_repository, key, value in self.__get_repository_from_foreign_keys(
+            partial_entity
+        ):
+            if not value and foreign_keys[key].nullable:
+                continue
+            await referred_repository.find_one_or_fail(str(value), db)
+
+        return True
+
+    @staticmethod
+    def __get_cascade_relations(entity: T) -> List[Any]:
+        cascade_relations = []
+
+        for relation in inspect(inspect(entity).class_).relationships:
+            delete_column = DatabaseUtils.get_column_represent_deleted(
+                get_columns(relation.mapper.class_)
+            )
+            if delete_column is None:
+                raise ValueError(f'Relation "{relation.key}" has no "deleted" column')
+
+            cr = getattr(entity, relation.key)
+            if cr and relation.cascade.delete_orphan:
+                if not isinstance(cr, List):
+                    cr = [cr]
+
+                cascade_relations.extend(
+                    cascade_relation
+                    for cascade_relation in cr
+                    if not getattr(cascade_relation, delete_column.name, True)
+                )
+
+        return cascade_relations
+
+    async def __soft_delete_cascade(
+        self, criteria: Union[str, int, FindOneOptions], db: Session
+    ) -> Optional[UpdateResult]:
+        entity = await self.find_one_or_fail(criteria, db)
+
+        rowcount = await self.__soft_delete_cascade_relations(entity, db)
+        rowcount += self.__soft_delete_entity(entity, db)
+
+        return UpdateResult(raw=[], affected=rowcount, generatedMaps=[])
+
+    async def __soft_delete_cascade_relations(self, entity: T, db: Session) -> int:
+        rowcount = 0
+
+        cascade_entities = self.__get_cascade_relations(entity)
+        for cascade_entity in cascade_entities:
+            result = await BaseRepository(inspect(cascade_entity).class_).__soft_delete_cascade(
+                str(cascade_entity.id), db
+            )
+            rowcount += result["affected"]
+
+        return rowcount
+
+    @staticmethod
+    def __soft_delete_entity(entity: T, db: Session) -> int:
+        entity_class = inspect(entity).class_
+        delete_column = DatabaseUtils.get_column_represent_deleted(get_columns(entity_class))
+        if delete_column is None:
+            raise ValueError(f'Entity "{entity_class.__name__}" has no "deleted" column')
+
+        if not getattr(entity, delete_column.name):
+            setattr(entity, delete_column.name, datetime.now())
+            db.flush()
+
+            return 1
+
+        return 0
