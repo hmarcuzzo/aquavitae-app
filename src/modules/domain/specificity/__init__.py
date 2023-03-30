2c3373e483cb7760bc0d5e8bac2219b99a819c59
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2023-03-30 17:31:08 +0100

    refactor: Removing unnecessary controllers

--- src/modules/domain/specificity/__init__.py
+++ src/modules/domain/specificity/__init__.py
@@ -4,10 +4,8 @@ from .entities.specificity_entity import Specificity
 from .controllers.specificity_controller import specificity_router
 
 from .entities.specificity_type_entity import SpecificityType
-from .controllers.specificity_type_controller import specificity_type_router
 
 specificity_routers = APIRouter()
 specificity_routers.include_router(specificity_router)
-specificity_routers.include_router(specificity_type_router)
 
 specificity_entities = [Specificity, SpecificityType]
